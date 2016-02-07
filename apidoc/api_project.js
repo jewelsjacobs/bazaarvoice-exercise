30d4ef5be6c03ea2957a630448db6da653b8c297
Julia Jacobs <juliajacobs@Jacobs-Family-Laptop.local>
2016-02-06 20:23:50 -0600

    Updates

---
+++ apidoc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "bazaarvoice-exercise",  "version": "1.0.0",  "description": "A full stack javascript hangman excercise bazaarvoice with some love",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-02-07T02:23:47.425Z",    "url": "http://apidocjs.com",    "version": "0.14.0"  }});
